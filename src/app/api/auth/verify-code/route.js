1425012b6f0b45b1eb9cef4e1389cef3df23f76f
Paula <167124547+ppaulig@users.noreply.github.com>
2025-03-24 22:05:09 -0300

    update verify-code

--- src/app/api/auth/verify-code/route.js
+++ src/app/api/auth/verify-code/route.js
@@ -4,13 +4,13 @@ import { NextResponse } from 'next/server';
 import db from '@/lib/db';
 import bcrypt from 'bcryptjs';
 
-// Verifica y cambia la contraseña:
+// Verifica:
 // Obtiene el codigo asociado al mail desde la base de datos
 // Lo compara con el codigo ingresado por el usuario
-// Si son iguales, modifica la contraseña
+// Si son iguales redirecciona a la pagina de actualizar usuario.
 export async function PUT(request) {
     try {
-        const { mail, codigo , nuevaContrasenia} = await request.json();
+        const { mail, codigo} = await request.json();
 
         const [rows] = await db.execute('SELECT codigo_recuperacion FROM usuarios_autorizados WHERE mail = ? AND codigo_recuperacion IS NOT NULL', [mail]);
 
@@ -18,17 +18,13 @@ export async function PUT(request) {
 
         const esIgual = await bcrypt.compare(codigo, codigoHasheado);
 
-        if (!esIgual) {
-            return NextResponse.json({message: 'Código incorrecto' });
+        if (esIgual) {
+            return //redireccionar;
+        } else {
+            return NextResponse.json({ success: false, message: "Código incorrecto" });
         }
 
-        const contraHasheada = await bcrypt.hash(nuevaContrasenia, 10);
-        
-        await db.execute('UPDATE usuarios_autorizados SET contraseña = ?, codigo_recuperacion = NULL WHERE mail = ?', [contraHasheada, mail]);
-
-        return NextResponse.json({message: 'Contraseña actualizada correctamente' });
-
     } catch (error) {
-        return NextResponse.json({message: 'Error al verificar el código o cambiar la contraseña' });
+        return NextResponse.json({ success: false, message: "Error al verificar el código" });
     }
 }
